415bbd7344d11e01854a235f16e329b79cc42a96
Manuel Clifton <manuclifton@gmail.com>
2020-03-11 15:23:27 -0300

    ajustes deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //crear un puerto al servidor
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -32,6 +32,6 @@ app.get('/', (req, res) =>{
 */
 
 //Arrancar la app
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
